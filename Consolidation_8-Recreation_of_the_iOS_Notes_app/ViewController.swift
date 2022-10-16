b76ef769b82318ef36a181113e36595a8dde67fa
EPJrWorld <edwin.przezwiecki.jr@icloud.com>
2022-10-16 11:19:19 +0200

    Initial Commit

---
+++ Consolidation_8-Recreation_of_the_iOS_Notes_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Consolidation_8-Recreation_of_the_iOS_Notes_app
+//
+//  Created by Edwin Przeźwiecki Jr. on 16/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
